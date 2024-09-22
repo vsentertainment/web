fc0b10387bb98f2d2e2ac04efc05333a6bfd282c
vsentertainment <vsmusicent@gmail.com>
2024-09-21 23:43:04 -0300

    video update

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,6 +1,9 @@
 const video = document.getElementById('videopl');
 const playPauseButton = document.getElementById('playPause');
 
+video.addEventListener('loadeddata', function () {
+    playPauseButton.style = '';
+});
 playPauseButton.addEventListener('click', () => {
     if (video.paused) {
         video.play();
